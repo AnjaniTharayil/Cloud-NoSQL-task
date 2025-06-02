a950886f62554b1aadec0b9c2034efc485ad6d31
Anjani Tharayil <anjani.tharayil@bt.com>
2025-06-02 21:27:36 +0530

    task 5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
+import ImportFileComponent from './components/fileUpload'; // Adjust the import path as necessary
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/" element={<ImportFileComponent />} />
+        {/* <Route path="/product/:id" element={<ProductDetails />} /> */}
       </Routes>
     </Router>
   );
